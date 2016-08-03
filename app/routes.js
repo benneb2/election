dc9da2067cf1b656cca949480f589dd0d12b81e5
benneb2 <benneb2@gmail.com>
2016-08-03 09:49:40 +0200

    tel

--- app/routes.js
+++ app/routes.js
@@ -660,7 +660,7 @@ app.all('/*', function(req, res, next) {
 							if(typeof results[i].resultStation == 'undefined')
 								results[i].resultStation = '';
 
-							csvData = csvData  + results[i].resultDate + ',' + results[i].resultStation + ',' + user + ',' + tel;
+							csvData = csvData  + results[i].resultDate + ',' + results[i].resultStation + ',' + user + ',0' + tel;
 							for(var j in sortQuestions)
 							{
 								csvData = csvData + ',';
@@ -679,6 +679,7 @@ app.all('/*', function(req, res, next) {
 						}
 						// tmpdata = tmpdata.substring(1);
 						
+						console.log(csvData);
 						// res.json(csvData);
 						res.writeHead(200,{'content-type':'text/csv'});
 		    			res.write(csvData);
